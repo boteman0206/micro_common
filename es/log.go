3cf4e9e2098f62c2bd89b30dd24f492a67fb0ae1
pengwei <pengw.rppet.cn>
2022-09-12 12:19:30 +0800

    update

--- es/log.go
+++ es/log.go
@@ -1,6 +1,98 @@
 package es
 
-/*
-	收集日志信息写入到es，可以先写入kafuka然后在写到es中
-	这里先直接写es，后续再接入中间件
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"runtime"
+)
+
+/**
+golang自带的log包使用：默认是打印时间+日志信息： 可以调整
 */
+var BufferLog *log.Logger // 定义的输出到buffer的日志,测试
+var LogBuf bytes.Buffer
+var client *DC_ES
+
+var MyLog *Log // 定义的输出到文件的日志
+type Log struct {
+	File string `json:"file"`
+	Line int    `json:"line"`
+}
+
+func init() {
+	// 方式一： 自定义log输出到buffer也可以定义到文件
+	BufferLog = log.New(&LogBuf, "[info: ]", log.LstdFlags)
+
+	// 方式二：测试输出到文件中
+	var err error
+	logFile, err = os.OpenFile(defaultLogFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		defaultLogFile = "./web.log"
+		logFile, err = os.OpenFile(defaultLogFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+		if err != nil {
+			log.Fatalf("create log file err %+v", err)
+		}
+	}
+
+	debugLogger = log.New(os.Stdout, preDebug, flag) // debug 控制台输出
+	infoLogger = log.New(logFile, preInfo, flag)     // 文件输出
+	warningLogger = log.New(logFile, preWarning, flag)
+	errorLogger = log.New(logFile, preError, flag)
+
+	MyLog = new(Log)
+
+	// 使用es
+	client = NewEsClient()
+}
+
+const (
+	flag       = log.Ldate | log.Ltime
+	preDebug   = "[DEBUG]"
+	preInfo    = "[INFO]"
+	preWarning = "[WARNING]"
+	preError   = "[ERROR]"
+)
+
+var (
+	logFile        io.Writer
+	debugLogger    *log.Logger
+	infoLogger     *log.Logger
+	warningLogger  *log.Logger
+	errorLogger    *log.Logger
+	defaultLogFile = "/var/log/web.log"
+)
+
+func (l *Log) Debug(v ...interface{}) {
+	name, line, _ := l.GetFileName(1)
+	debugLogger.Print(name, line, v)
+
+}
+
+func (l *Log) Info(v ...interface{}) {
+	name, line, _ := l.GetFileName(1)
+	buffer := client.GetBuffer(name, line, v)
+	client.Add(buffer)
+
+	infoLogger.Print(v...)
+}
+
+func (l *Log) Warning(v ...interface{}) {
+	name, line, _ := l.GetFileName(1)
+	buffer := client.GetBuffer(name, line, v)
+	client.Add(buffer)
+	warningLogger.Print(v...)
+}
+
+func (l *Log) Error(v ...interface{}) {
+	name, line, _ := l.GetFileName(1)
+	buffer := client.GetBuffer(name, line, v)
+	client.Add(buffer)
+	errorLogger.Print(v...)
+}
+
+func (l *Log) GetFileName(skip int) (file string, line int, ok bool) {
+	_, file, line, ok = runtime.Caller(skip + 1) // 获取调用之前的路径
+	return
+}
